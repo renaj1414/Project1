257eadcd103f0d2218535496b8361d1e223d61ac
sejon <sejon@LAPTOP-GI3U8FMJ.fios-router.home>
2023-04-07 13:48:15 -0400

    Initial commit

--- src/project1/Project1.java
+++ src/project1/Project1.java
@@ -5,6 +5,7 @@ public class Project1 {
 	public static void main(String args []) {
 		System.out.println("Serena ");
 		System.out.println("Serena");
+		System.out.println("My name is Serena.p");
 	}
 
 }
